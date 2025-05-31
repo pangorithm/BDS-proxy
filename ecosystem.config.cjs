d0bc4a86864bbd8d4d1fd115d863bac99f07fc42
pangorithm <phs559800@gmail.com>
2025-05-31 20:07:15 +0900

    단순 proxy 처리 용도로 nestjs는 오버 엔지니어링인 관계로 구조 단순화

---
+++ ecosystem.config.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'bds-proxy',
+      script: './main.js',
+      exec_mode: 'fork',
+      args: [
+        '--host',
+        '0.0.0.0',
+        '--port',
+        29132,
+        '--destination_host',
+        '127.0.0.1',
+        '--destination_port',
+        19132,
+      ],
+    },
+  ],
+};
---
+++ main.js
@@ -0,0 +1,94 @@
+import { Command } from 'commander';
+import { Relay } from 'bedrock-protocol';
+
+function initializeOptions() {
+  const program = new Command();
+  program
+    .name('bedrock-proxy')
+    .description('A simple Bedrock proxy server')
+    .version('1.0.0');
+
+  program
+    .option('-h, --host <host>', 'Host to bind to', '127.0.0.1')
+    .option('-p, --port <port>', 'Port to listen on', '29132')
+    .option(
+      '-H, --destination_host <destination_host>',
+      'Destination Host to bind to',
+      '127.0.0.1',
+    )
+    .option(
+      '-P, --destination_port <destination_port>',
+      'Destination Port to bind to',
+      '19132',
+    );
+
+  return program.parse().opts();
+}
+
+function main({ host, port, destination_host, destination_port }) {
+  console.log('Starting proxy with config:', {
+    host,
+    port,
+    destination_host,
+    destination_port,
+  });
+
+  try {
+    const relay = new Relay({
+      /* The version defaults to the latest version supported by bedrock-protocol.
+       You can specify a specific version if needed, e.g., '1.21.80' */
+      // version: '1.21.80',
+
+      /* host and port to listen for clients on */
+      host: host,
+      port: parseInt(port),
+      offline: true,
+
+      /* Where to send upstream packets to */
+      destination: {
+        host: destination_host,
+        port: parseInt(destination_port),
+      },
+    });
+    relay.listen(); // Tell the server to start listening.
+
+    relay.on('connect', (player) => {
+      try {
+        console.log('New connection', player.connection.address);
+
+        // Server is sending a message to the client.
+        player.on('clientbound', ({ name, params }, des) => {
+          if (name === 'disconnect') {
+            // Intercept kick
+            params.message = 'Intercepted'; // Change kick message to "Intercepted"
+          }
+        });
+        // Client is sending a message to the server
+        player.on('serverbound', ({ name, params }, des) => {
+          if (name === 'text') {
+            // Intercept chat message to server and append time.
+            params.message += `, on ${new Date().toLocaleString()}`;
+          }
+
+          if (name === 'command_request') {
+            // Intercept command request to server and cancel if its "/test"
+            if (params.command == '/test') {
+              des.canceled = true;
+            }
+          }
+        });
+      } catch (error) {
+        console.error('Error handling player connection:', error);
+      }
+    });
+  } catch (error) {
+    console.error('Failed to create relay:', error);
+  }
+
+  console.log(`proxy server started`);
+}
+
+const options = initializeOptions();
+console.log(`Options: ${JSON.stringify(options, null, 2)}`);
+
+main(options);
